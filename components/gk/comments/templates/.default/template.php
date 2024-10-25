9a006a45b9bdeae8df26c6ab60cfe36bb9d65cb7
Pavel <boss.ug93@gmail.com>
2024-10-25 16:00:01 +0300

    добавление верстки, блок кнопок, компонент списка

--- components/gk/comments/templates/.default/template.php
+++ components/gk/comments/templates/.default/template.php
@@ -3,18 +3,22 @@ if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die();
 use Bitrix\Main\Localization\Loc;
 Loc::loadLanguageFile(__FILE__);
 CJSCore::Init(['fx']);
-\Bitrix\Main\UI\Extension::load(['ui.buttons', 'ui.dialogs.messagebox', 'ui.layout-form', 'gkvue3.comments']);
+\Bitrix\Main\UI\Extension::load(['ui.buttons', 'ui.dialogs.messagebox', 'ui.layout-form', 'gkvue3.comments','ui.bootstrap4']);
 ?>
 <div id="application">
     <content-header v-bind:title="modalTitle"></content-header>
-    <content-body v-for="(post,index) in posts"
-        :key="index"
-        :title="post.title"
-        :text="post.text"
-        :icon="post.icon"
-        :elementId="post.elementId"
-        :data="post.data"
-    ></content-body>
+    <div class="comment-body">
+        <content-body v-for="(post,index) in posts"
+            :key="index"
+            :name="post.title"
+            :text="post.text"
+            :icon="post.icon"
+            :elementid="post.elementId"
+            :data="post.data"
+            :timedata="post.timeData"
+            :letter="post.letter"
+        ></content-body>
+    </div>
 </div>
 <script>
     const comments = new BX.Comments('#application');
