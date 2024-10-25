971f3846bc60a5f44703152d3069dd7f0897053e
Pavel <boss.ug93@gmail.com>
2024-10-25 16:48:33 +0300

    изменен getmap таблицы

--- modules/gk.comments/lib/orm/gkcommentstable.php
+++ modules/gk.comments/lib/orm/gkcommentstable.php
@@ -12,6 +12,7 @@
     use Bitrix\Main\ORM\Fields\DatetimeField;
     use Bitrix\Main\ORM\Fields\IntegerField;
     use Bitrix\Main\ORM\Fields\StringField;
+    use Bitrix\Main\ORM\Fields\BooleanField;
     use Bitrix\Main\SystemException;
 
     Loc::loadMessages(__FILE__);
@@ -57,10 +58,52 @@
                         'autocomplete' => true,
                     ]
                 ),
+
+                /** ID элемента, если на детальной странице */
                 new IntegerField('ELEMENT_ID', ['required' => true]),
+
+                /** Код инфоблока */
+                new StringField('IBLOCK_CODE'),
+
+                /** ID инфоблока */
+                new IntegerField('IBLOCK_ID'),
+
+                /** Если не новое сообщение, а ответ на сообщение */
                 new IntegerField('PARENT_ELEMENT_ID'),
+
+                /** Время создания сообщения */
                 new DatetimeField('DATE_CREATE', ['required' => true]),
+
+                /** Текст сообщения */
                 new StringField('COMMENT'),
+
+                /** Если пользователь зарегистрирован и авторизован */
+                new IntegerField('USER_ID'),
+
+                /** Если пользователь не авторизован, заполняются поля в форме */
+                new StringField('USER_NAME'),
+                new StringField('USER_LAST_NAME'),
+                new StringField('USER_EMAIL'),
+
+                /** Показывать сообщение да/нет */
+                new BooleanField('SHOW',[
+                    'values' => ['Y', 'N'],
+                    'default_value' => 'Y',
+                    'save_data_modification' => function () {
+                        return [
+                            function ($value) {
+                                return $value === true ? 'Y' : 'N';
+                            }
+                        ];
+                    },
+                    'fetch_data_modification' => function () {
+                        return [
+                            function ($value) {
+                                return $value === 'Y';
+                            }
+                        ];
+                    }
+                ])
             ];
         }
     }
